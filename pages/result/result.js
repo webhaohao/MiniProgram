abb7d61a2fe56e0daa25b560777c893b5918083b
haohao <734714852@qq.com>
2019-06-06 19:41:23 +0800

    20190606

--- pages/result/result.js
+++ pages/result/result.js
@@ -6,13 +6,16 @@ Page({
    */
   data: {
         info:'',
-        canvasHidden:true
+        canvasHidden:true,
+        shareImgSrc:'../images/bg3.png',
+        titlePath:'../images/title.png'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+        var that = this;
         wx.setNavigationBarTitle({
           title: "清华经管学院EMBA"
         })
@@ -93,6 +96,61 @@ Page({
   },
   //保存至相册
   saveImageToPhotosAlbum: function () {
-
-  }
+      var that = this;
+      const ctx = wx.createCanvasContext('share');
+      var bgImgPath = that.data.shareImgSrc;
+      var titlePath = that.data.titlePath;
+      ctx.drawImage(bgImgPath, 0, 0, 750, 1334);
+      ctx.drawImage(titlePath,20,174,690,206);
+      ctx.draw(false, function() {
+        // 3. canvas画布转成图片
+        wx.canvasToTempFilePath({
+          x: 0,
+          y: 0,
+          width: 750,
+          height: 1334,
+          destWidth: 375,
+          destHeight: 667,
+          canvasId: 'share',
+          success: function (res) {
+            console.log(res);
+            that.setData({
+              shareImgSrc: res.tempFilePath
+            })
+            if (!res.tempFilePath) {
+              wx.showModal({
+                title: '提示',
+                content: '图片绘制中，请稍后重试',
+                showCancel: false
+              })
+            }
+            //4. 当用户点击分享到朋友圈时，将图片保存到相册
+            wx.saveImageToPhotosAlbum({
+              filePath: that.data.shareImgSrc,
+              success(res) {
+                console.log(res);
+                wx.showModal({
+                  title: '图片保存成功',
+                  content: '图片成功保存到相册了，去发圈噻~',
+                  showCancel: false,
+                  confirmText: '好哒',
+                  confirmColor: '#72B9C3',
+                  success: function (res) {
+                    if (res.confirm) {
+                      console.log('用户点击确定');
+                    }
+                    that.setData({
+                      canvasHidden: true
+                    })
+                  }
+                })
+              }
+            })
+          },
+          fail: function (res) {
+            console.log(res)
+          }
+        })
+      });
+  } 
 })
